91f1157b64dd8ce8fc74919a45f694bf63fc8c9b
Tolyan2003 <ttl2003.al@gmail.com>
2024-12-05 15:47:48 +0300

    Контрольная работа

---
+++ 06K_lesson/test_02_calc.py
@@ -0,0 +1,42 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+# Фикстура для создания WebDriver
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    yield driver
+    driver.quit()
+
+# test
+def test_02_calc(driver):
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    driver.fullscreen_window()
+
+    # Ввод значения задержки
+    delay = driver.find_element(By.CSS_SELECTOR, "#delay")
+    delay.clear()
+    delay.send_keys("45")
+
+    # Нажатие на кнопки
+    seven_button = driver.find_element(By.XPATH, "//span[.='7']")
+    plus_button = driver.find_element(By.XPATH, "//span[.='+']")
+    eight_button = driver.find_element(By.XPATH, "//span[.='8']")
+    equals_button = driver.find_element(By.XPATH, "//span[.='=']")
+
+    seven_button.click()
+    plus_button.click()
+    eight_button.click()
+    equals_button.click()
+
+    # Ожидание результата
+
+    result = WebDriverWait(driver, 50).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15"))
+
+    # Проверка результата
+    assert result, "Результат не отображается!"
---
+++ 06K_lesson/test_03_shop.py
@@ -0,0 +1,63 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+# Фикстура для создания WebDriver
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    yield driver
+    driver.quit()
+
+# test
+def test_03_shop(driver):
+    driver.get("https://www.saucedemo.com/")
+    driver.fullscreen_window()
+
+    # Авторизация
+    username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#user-name")))
+    password_field = driver.find_element(By.CSS_SELECTOR, "#password")
+    login_button = driver.find_element(By.CSS_SELECTOR, "#login-button")
+
+    username_field.send_keys("standard_user")
+    password_field.send_keys("secret_sauce")
+    login_button.click()
+
+    # Добавление товаров в корзину
+    add_backpack_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack")))
+    add_tshirt_button = driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt")
+    add_onesie_button = driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie")
+
+    add_backpack_button.click()
+    add_tshirt_button.click()
+    add_onesie_button.click()
+
+    # Переход в корзину
+    cart_icon = driver.find_element(By.CSS_SELECTOR, ".shopping_cart_link")
+    cart_icon.click()
+
+    # Перейти к оформлению заказа
+    checkout_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#checkout")))
+    checkout_button.click()
+
+    # Заполнить информацию о доставке
+    first_name_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "first-name")))
+    last_name_field = driver.find_element(By.ID, "last-name")
+    postal_code_field = driver.find_element(By.ID, "postal-code")
+    continue_button = driver.find_element(By.ID, "continue")
+
+    first_name_field.send_keys("Иван")
+    last_name_field.send_keys("Иванов")
+    postal_code_field.send_keys("101001")
+    continue_button.click()
+
+    # Прочитать итоговую сумму
+    total_price = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "summary_total_label"))).text
+
+    # Проверка итоговой суммы
+    assert total_price == "Total: $58.29", f"Итоговая сумма не соответствует. Ожидается $58.29, фактически {total_price}."
