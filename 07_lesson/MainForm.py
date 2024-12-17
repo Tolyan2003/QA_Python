07987a7af952a05fe48beac1b1f48101f7226a88
Tolyan2003 <ttl2003.al@gmail.com>
2024-12-17 09:26:18 +0300

    Домашняя работа №7 с доработками

--- 07_lesson/MainForm.py
+++ 07_lesson/MainForm.py
@@ -1,32 +1,38 @@
 from selenium.webdriver.common.by import By
 
 class MainForm:
+    URL = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+
     def __init__(self, driver):
-        self._driver = driver
+        self.driver = driver
 
     def webpage(self):
-        self._driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
-        self._driver.fullscreen_window()
-        self._driver.implicitly_wait(4)
-        self._driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        self.driver.get(self.URL)
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(4)
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
     def send_form(self, first_name, last_name, address, email, phone_number, zip_code, city, country, job_position, company):
         # Заполнение формы
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='first-name']").send_keys(first_name)
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='last-name']").send_keys(last_name)
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='address']").send_keys(address)
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='e-mail']").send_keys(email)
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='phone']").send_keys(phone_number)
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='zip-code']").send_keys(zip_code)
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='city']").send_keys(city)
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='country']").send_keys(country)
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='job-position']").send_keys(job_position)
-        self._driver.find_element(By.CSS_SELECTOR, "input[name='company']").send_keys(company)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='first-name']").send_keys(first_name)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='last-name']").send_keys(last_name)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='address']").send_keys(address)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='e-mail']").send_keys(email)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='phone']").send_keys(phone_number)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='zip-code']").send_keys(zip_code)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='city']").send_keys(city)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='country']").send_keys(country)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='job-position']").send_keys(job_position)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='company']").send_keys(company)
 
 
-    def click(self):
+    def submit(self):
         # Нажатие на кнопку
-        self._driver.find_element(By.CSS_SELECTOR, "[type='submit'").click()
+        self.driver.find_element(By.CSS_SELECTOR, "[type='submit'").click()
+
+    def get_field_background_color(self, locator):
+        element = self.driver.find_element(By.ID, locator)
+        return element.value_of_css_property('background-color')
 
     def close_browser(self):
-        self._driver.quit()
+        self.driver.quit()
